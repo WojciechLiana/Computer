0b586667e49c1b0ac367757ff5b7a9ef0f7bf037
Wojciech Liana <wojtek.liana@gmail.com>
2020-03-22 13:16:27 +0100

    computer basic version

--- src/Computer.js
+++ src/Computer.js
@@ -20,14 +20,14 @@ function Menu() {
 
 function Content() {
 
-    const processorList = [['Choose part', ''], ['intel i3', 1000], ['intel i5', 1200], ['intel i7', 1500], ['intel i9', 1900], ['AMD K5', 1000], ['AMD K6', 1200],
-        ['AMD K7', 1500], ['AMD K8', 1900]];
-    const cardsList = [['Choose part', ''], ['GeForce 700', 700], ['GeForce 800', 800], ['GeForce 900', 900], ['GeForce 1000', 1000], ['Radeon Rx 2xx', 700],
-        ['Radeon Rx 3xx', 800], ['Radeon Rx 4xx', 900], ['Radeon Rx 5xx', 1000]];
-    const ramList = [['Choose part', ''], ['4 GB', 200], ['8 GB', 600], ['16 GB', 1000]];
-    const powerSupplyList = [['Choose part', ''], ['500 W', 200], ['600 W', 300], ['700 W', 400], ['800 W', 500], ['1000 W', 600]];
-    const motherboardList = [['Choose part', ''], ['MSI', 500], ['Gigabyte', 600], ['Asus', 500], ['ASRock', 600]];
-    const hardDriveList = [['Choose part', ''], ['200 GB', 400], ['500 GB', 500], ['800 GB', 600], ['1TB', 700]];
+    const processorList = [['Choose part ', ''], ['intel i3 ', 1000], ['intel i5 ', 1200], ['intel i7 ', 1500], ['intel i9 ', 1900], ['AMD K5 ', 1000], ['AMD K6 ', 1200],
+        ['AMD K7 ', 1500], ['AMD K8 ', 1900]];
+    const cardsList = [['Choose part ', ''], ['GeForce 700 ', 700], ['GeForce 800 ', 800], ['GeForce 900 ', 900], ['GeForce 1000 ', 1000], ['Radeon Rx 2xx ', 700],
+        ['Radeon Rx 3xx ', 800], ['Radeon Rx 4xx ', 900], ['Radeon Rx 5xx ', 1000]];
+    const ramList = [['Choose part ', ''], ['4 GB ', 200], ['8 GB ', 600], ['16 GB ', 1000]];
+    const powerSupplyList = [['Choose part ', ''], ['500 W ', 200], ['600 W ', 300], ['700 W ', 400], ['800 W ', 500], ['1000 W ', 600]];
+    const motherboardList = [['Choose part ', ''], ['MSI ', 500], ['Gigabyte ', 600], ['Asus ', 500], ['ASRock ', 600]];
+    const hardDriveList = [['Choose part ', ''], ['200 GB ', 400], ['500 GB ', 500], ['800 GB ', 600], ['1TB ', 700]];
 
     const database = [processorList, cardsList, ramList, powerSupplyList, motherboardList, hardDriveList];
 
@@ -53,7 +53,7 @@ function Content() {
 function Form({database, partsID, hooks, names}) {
 
     return (
-        <form>
+        <form className='form'>
             {names.map((element, id) => <div key={id}>Choose {names[id]} for your PC <ComputerPart
                 database={database[id]}
                 partsID={partsID[id]}
@@ -76,15 +76,37 @@ function ComputerPart({database, partsID, hooks}) {
 function Cart({database, partsID, hooks}) {
 
     return (
-        <div className='cart'>{database.map((element, id) => <div key={id}>
+        <div className='cart'>
+            <NumberOfSelectedParts parts={partsID}/>
+            {database.map((element, id) => (<div key={id}>
             <span>{element[partsID[id]]}</span><RemoveFromCartButton partsID={partsID} onClick={(e) => hooks[id](e)}/>
-        </div>)}</div>
+            </div>))}
+            <Price part={database} partId={partsID}/>
+        </div>
     );
 }
 
 function RemoveFromCartButton({onClick, partsID}) {
     return (
         <button onClick={() => onClick(0)}>X</button>
+    );
+}
+
+function Price({part, partId}){
+
+    const price2 = partId.map((element, id)=>(element == 0 ? 0 : part[id][element][1])).reduce((acc, curr)=> acc + curr);
+
+    return(
+        <div>Total price: {price2} zl</div>
+    );
+}
+
+function NumberOfSelectedParts({parts}){
+
+    const numberOfParts = parts.filter(element=>element>0).length;
+
+    return(
+        <div>Parts in cart: {numberOfParts}</div>
     );
 }
 
